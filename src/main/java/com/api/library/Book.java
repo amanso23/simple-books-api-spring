57f4a2877f04069a8a93064fc2809390f6a3cb21
Adrian <adrianmanso80@gmail.com>
2024-11-13 13:43:15 +0000

    remove unused classes

--- src/main/java/com/api/library/Book.java
+++
@@ -1,5 +0,0 @@
-package com.api.library;
-
-public class Book {
-
-}
--- src/main/java/com/api/library/dao/BookDao.java
+++ src/main/java/com/api/library/dao/BookDao.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 
 import com.api.library.model.Book;
 
@@ -20,3 +18,5 @@ public interface BookDao extends JpaRepository<Book, Long> {
     public Book findBookByAuthor(String author);
 
 }
+
+
